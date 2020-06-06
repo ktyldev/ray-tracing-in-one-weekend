b827c777e014574879257db098a7b9b2a9f325ee
K Tyl <me@ktyl.dev>
2020-06-06 23:25:36 +0100

    improve diffuse approximation

--- src/main.cpp
+++ src/main.cpp
@@ -21,9 +21,9 @@ colour ray_colour(const ray& r, const hittable& world, int depth)
         return colour(0,0,0);
     }
 
-    if (world.hit(r, 0, infinity, rec))
+    if (world.hit(r, 0.001, infinity, rec))
     {
-        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        point3 target = rec.p + rec.normal + random_in_hemisphere(rec.normal);
         return 0.5 * ray_colour(ray(rec.p, target - rec.p), world, depth-1);
     }
 
